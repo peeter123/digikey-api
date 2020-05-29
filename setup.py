a63ee9ba25448fdf47f445c99b3c7451f0e3f45b
Peter Oostewechel <peter_oostewechel@hotmail.com>
2020-05-29 14:54:33 +0200

    Release 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="digikey-api",
-    version="0.2.1",
+    version="0.2.2",
     author="Peter Oostewechel",
     author_email="peter_oostewechel@hotmail.com",
     license="GPL v3",
