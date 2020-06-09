e3914f02478107dc7f83aa34c535e7e5a457d7c0
Peter Oostewechel <peter_oostewechel@hotmail.com>
2020-06-09 21:30:15 +0200

    Use decorator for easy wrapping of API functions

--- digikey/__init__.py
+++ digikey/__init__.py
@@ -1,6 +1,6 @@
 import logging
 from digikey.v2.api import (search, part)
-from digikey.v3.api import (searchv3, partv3)
+from digikey.v3.api import (keyword_search, part_number)
 
 logger = logging.getLogger(__name__)
 
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 def setup_logger(logger_ref):
     logger_ref.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+        '%(asctime)s - %(name)s - %(levelname)6s: %(message)s')
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger_ref.addHandler(handler)
--- digikey/v3/api.py
+++ digikey/v3/api.py
@@ -1,33 +1,69 @@
+import os
+import types
+import wrapt
+import logging
 import digikey.v3.product_information_client as pic
-from digikey.v3.productinformation import KeywordSearchRequest, KeywordSearchResponse, ProductDetails
+import digikey.oauth.oauth2
+import digikey.v3.productinformation as dpi
+from digikey.v3.productinformation import KeywordSearchRequest, KeywordSearchResponse, ProductDetails, PartSearchApi
+from digikey.v3.productinformation.rest import ApiException
+
+logger = logging.getLogger(__name__)
 
 
-def searchv3(search_request: KeywordSearchRequest) -> KeywordSearchResponse:
-    """
-    Search Digikey for a general keyword (and optional filters).
-    Args:
-        query (str): Free-form keyword query
-        start: Ordinal position of first result
-        limit: Maximum number of results to return
-    Returns:
-        list of `models.KeywordSearchResult` objects.
-    """
-    client = pic.ProductInformation()
-    response = client.keyword_search(search_request)
-    return response
+class ProductApiWrapper(object):
+    def __init__(self, wrapped_function):
+        # Configure API key authorization: apiKeySecurity
+        configuration = dpi.Configuration()
+        configuration.api_key['X-DIGIKEY-Client-Id'] = os.getenv('DIGIKEY_CLIENT_ID')
+        # Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+        # configuration.api_key_prefix['X-DIGIKEY-Client-Id'] = 'Bearer'
+
+        # Configure OAuth2 access token for authorization: oauth2AccessCodeSecurity
+        configuration = dpi.Configuration()
+        self._digikeyApiTokenObject = digikey.oauth.oauth2.TokenHandler(version=3).get_access_token()
+        configuration.access_token = self._digikeyApiTokenObject.access_token
+
+        # create an instance of the API class
+        self._api_instance = dpi.PartSearchApi(dpi.ApiClient(configuration))
+
+        # Populate reused ids
+        self.authorization = self._digikeyApiTokenObject.get_authorization()
+        self.x_digikey_client_id = os.getenv('DIGIKEY_CLIENT_ID')
+
+        self.wrapped_function = wrapped_function
+
+    @staticmethod
+    def _print_remaining_requests(header):
+        try:
+            rate_limit = header['X-RateLimit-Limit']
+            rate_limit_rem = header['X-RateLimit-Remaining']
+            logger.debug('Requests remaining: [{}/{}]'.format(rate_limit_rem, rate_limit))
+        except KeyError:
+            pass
+
+    @wrapt.decorator
+    def __call__(self, wrapped, instance, args, kwargs):
+        try:
+            func = getattr(self._api_instance, self.wrapped_function)
+            wrapped(*args, **kwargs)
+            api_response = func(*args, self.authorization, self.x_digikey_client_id, **kwargs)
+            self._print_remaining_requests(api_response[2])
+            return api_response[0]
+        except ApiException as e:
+            logger.error(f'Exception when calling {self.wrapped_function}: {e}')
 
 
-def partv3(partnr: str) -> ProductDetails:
-    """
-    Query part by unique ID
-    Args:
-        partnr (str): Part number. Works best with Digi-Key part numbers.
-        include_associated (bool): The option to include all Associated products
-        include_for_use_with (bool): The option to include all For Use With product
-    Kwargs:
-    Returns:
-        dict. See `models.Part` for exact fields.
-    """
-    client = pic.ProductInformation()
-    response = client.product_details(partnr)
-    return response
+@ProductApiWrapper('products_keyword_post_with_http_info')
+def keyword_search(**kwargs) -> KeywordSearchResponse:
+    if 'body' in kwargs:
+        logger.debug('CALL -> keyword_search')
+        logger.info(f'Search for: {kwargs["body"].keywords}')
+
+
+@ProductApiWrapper('products_digi_key_part_number_get_with_http_info')
+def part_number(*args, **kwargs) -> ProductDetails:
+    if len(args):
+        logger.debug('CALL -> part_number')
+        logger.info(f'Get product details for: {args[0]}')
+
