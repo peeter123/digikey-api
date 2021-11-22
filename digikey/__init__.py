7819e5daeecd62fbe5ab0e1dc5d80620edd1cb70
Electro707 <general@electro707.com>
2021-11-22 15:14:33 -0500

    Removed forced logging

--- digikey/__init__.py
+++ digikey/__init__.py
@@ -1,20 +1,5 @@
 import logging
-from digikey.v2.api import (search, part)
 from digikey.v3.api import DigikeyAPI
 from digikey.configfile import (DigikeyBaseConfig, DigikeyJsonConfig)
-
-logger = logging.getLogger(__name__)
-
-
-def setup_logger(logger_ref):
-    logger_ref.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)20.20s - %(levelname)8s: %(message)s')
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logger_ref.addHandler(handler)
-
-
-setup_logger(logger)
 
 name = 'digikey'
