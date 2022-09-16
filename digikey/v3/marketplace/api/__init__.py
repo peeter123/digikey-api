df18c901124c63d70302f85c68d7d5ffcd803139
Jeff Johnson <jeff.johnson@auros.global>
2022-09-16 12:38:02 -0400

    * added swagger generated marketplace api

---
+++ digikey/v3/marketplace/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from digikey.v3.marketplace.api.health_check_api import HealthCheckApi
+from digikey.v3.marketplace.api.orders_api import OrdersApi
